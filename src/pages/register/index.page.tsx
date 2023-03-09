5941ee3b7861c937494a66ececddbc803ecb231b
Macsuel Dias <macsueldias@hotmail.com>
2023-03-08 23:23:28 -0300

    feat: page connect-calendar

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -45,6 +45,7 @@ export default function Register() {
         name: data.name,
         username: data.username,
       })
+      await router.push('/register/connect-calendar')
     } catch (err: any) {
       if (err?.response?.data?.message) {
         alert(err.response.data.message)
